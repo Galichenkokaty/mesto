5969fe0e406c1295025e4f258257e43ed06af430
Galichenkokaty <galichencko.katerina@yandex.ru>
2022-05-20 18:46:28 +0300

    исправлены ошибки в разметке и скрипте

--- script/index.js
+++ script/index.js
@@ -2,8 +2,8 @@ let openPopup = document.querySelector('.profile__edit');
 let closePopup = document.querySelector('.popup__close');
 let popup = document.querySelector('.popup');
 let formElement = document.querySelector('.popup__container');
-let nameInput = document.querySelector('.popup__input_name');
-let jobInput = document.querySelector('.popup__input_information');
+let nameInput = document.querySelector('.popup__input_line_name');
+let jobInput = document.querySelector('.popup__input_line_information');
 let name = document.querySelector('.profile__title');
 let info = document.querySelector('.profile__subtitle');
 
@@ -24,8 +24,8 @@ function popupClose() {
 function formSubmitHandler(evt) {
     evt.preventDefault();
 
-    name = nameInput.textContent;
-    info = jobInput.textContent;
+    name.textContent = nameInput.value;
+    info.textContent = jobInput.value;
     popupClose();
 }
 formElement.addEventListener('submit', formSubmitHandler);
